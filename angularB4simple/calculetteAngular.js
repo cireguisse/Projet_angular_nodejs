536371b6e9fdbb4a59979c674570009ee2d3fe60
Mamadou cire <cireguisse95@gmail.com>
2018-03-28 13:36:56 +0200

    Add files via upload

---
+++ angularB4simple/calculetteAngular.js
@@ -0,0 +1,130 @@
+/* 
+ * Copyright (C) 2018 Rémi Venant $lt;remi.venant@gmail.com$gt;.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+
+/*
+ * Déclaration du module applicatif 'calculetteMiageApp' 
+ * Contient l'ensemble des composants de l'application calculette
+ */
+angular.module("calculetteMiageApp", [])
+        .service("OperationsHistoryService", [function () {
+                this.history = [];
+                this.currentOperation = {};
+
+                this.computeOperation = function (operator) {
+                    if (angular.isUndefined(operator)) {
+                        console.warn("missing operator to compute operation");
+                        return;
+                    }
+                    var op1 = parseFloat(this.currentOperation.op1);
+                    var op2 = parseFloat(this.currentOperation.op2);
+                    if (isNaN(op1) || isNaN(op2)) {
+                        console.warn("wrong operands");
+                        return;
+                    }
+                    var res;
+                    switch (operator) {
+                        case "+":
+                            res = op1 + op2;
+                            break;
+                        case "-":
+                            res = op1 - op2;
+                            break;
+                        case "/":
+                            if (op2 === 0.) {
+                                console.warn("cannot divide by 0");
+                                return;
+                            }
+                            res = op1 / op2;
+                            break;
+                        case "*":
+                            res = op1 * op2;
+                            break;
+                        default :
+                            console.warn("Wrong operator " + operator);
+                            return;
+                    }
+                    //Update currentOperation
+                    this.currentOperation.op1 = op1;
+                    this.currentOperation.op2 = op2;
+                    this.currentOperation.operator = operator;
+                    this.currentOperation.res = res;
+                    //Sauvegarde une copie de l'operation dans l'historique
+                    this.history.push(angular.copy(this.currentOperation));
+                    //retourne le resultat
+                    return res;
+                };
+
+                this.clearHistory = function () {
+                    this.history.length = 0;
+                };
+
+                this.loadMemory = function (memory) {
+                    this.currentOperation.op1 = memory;
+                }
+
+                //Charge une operation dans l'operation courante
+                this.loadOperation = function (operation) {
+                    this.currentOperation.op1 = operation.op1;
+                    this.currentOperation.op2 = operation.op2;
+                    this.currentOperation.operator = operation.operator;
+                    this.currentOperation.res = operation.res;
+                };
+            }])
+        .controller("CalculetteCtrl", ["$scope", "OperationsHistoryService", function ($scope, OHSvc) {
+                //Initialisation des donnes : on fait un lien entre le scope et l'operation courante du service d'historique
+                $scope.operation = OHSvc.currentOperation;
+
+                //Fonction à appeler depuis le DOM pour le calcul de l'operation
+                $scope.computeOperation = function (operator) {
+                    OHSvc.computeOperation(operator);
+                };
+            }])
+        .controller("MemoryCtrl", ["$scope", "OperationsHistoryService", function ($scope, OHSvc) {
+                //Initialisation des donnees 
+                $scope.data = {memory: null};
+
+                //retourne vrai si il existe un resultat dans l'operation courante
+                $scope.canSaveMemory = function () {
+                    return angular.isDefined(OHSvc.currentOperation.res);
+                };
+
+                //Sauvegarde la memoire
+                $scope.saveMemory = function () {
+                    $scope.data.memory = OHSvc.currentOperation.res;
+                };
+
+                //Charge la mémoire
+                $scope.loadMemory = function () {
+                    OHSvc.currentOperation.op1 = $scope.data.memory;
+                };
+            }])
+        .controller("HistoryCtrl", ["$scope", "OperationsHistoryService", function ($scope, OHSvc) {
+                //Initialisation des donnees
+                $scope.history = OHSvc.history;
+
+                //Charge une operation de l'historique
+                $scope.loadOperation = function (operation) {
+                    OHSvc.loadOperation(operation);
+                };
+                
+                //Efface l'historique
+                $scope.resetHistory = function(){
+                    OHSvc.clearHistory();
+                };
+            }]);
